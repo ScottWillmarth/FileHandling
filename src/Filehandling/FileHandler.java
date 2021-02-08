93ee7dae941dbe0abbfe34fc78f4d6016d6aa6ce
Scott Willmarth <scott.willmarth@hcl.com>
2021-02-08 12:09:16 -0500

    added reader method

--- src/Filehandling/FileHandler.java
+++ src/Filehandling/FileHandler.java
@@ -1,11 +1,56 @@
 package Filehandling; 
 import java.io.*;
+import java.util.Scanner;
 
 public class FileHandler {
+	
+	public static void readFile(File f)
+	{
+		BufferedReader in;
+		try 
+		{
+			in = new BufferedReader(new FileReader(f));
+			
+			String s = in.readLine();
+			if(s == null)
+			{
+				System.out.println("File is empty");
+			}
+			while(s != null)
+			{
+				System.out.println("Read: " + s);
+				s = in.readLine();
+			}
+			in.close();
+		}
+		catch (IOException e) {e.printStackTrace();}
+	}
 
 	public static void main(String[] args) 
 	{
-		// TODO Auto-generated method stub
+		@SuppressWarnings("resource")
+		Scanner myScan = new Scanner(System.in);
+		
+		System.out.println("Please type in a file to use (Remember to add the file extension)");
+	    String input = myScan.next();
+	    File f = null;
+	    try
+	    { 
+            f = new File(input); 
+  
+            if (f.createNewFile())
+            {
+            	System.out.println("File created"); 
+            }
+                
+            else
+            { 
+            	System.out.println("File already exists"); 
+            }
+        } 
+        catch (Exception e) {System.err.println(e);} 
+		
+		readFile(f);
 
 	}
 
